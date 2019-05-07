7ea9120080ce2f1e075484e4593bc92c3ebf9601
M4DG4M3R <m4d.g4m3r.indie.game.developer@gmail.com>
2019-05-07 21:25:39 +0200

    9.1 Minimap

---
+++ MapCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour
+{
+    private Transform mtransform;
+    public Transform FollowCamera;
+
+    private void Awake()
+    {
+        mtransform = this.transform;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 position = mtransform.position;
+        Vector3 targetPosition = FollowCamera.position;
+
+        Vector3 nextPosition = new Vector3(targetPosition.x, position.y, targetPosition.z);
+
+        this.mtransform.position = nextPosition;
+
+
+    }
+}
